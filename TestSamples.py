3d771476b8e2e2119aee7c7ee2bc1800be8a1fc5
Elliot Pourmand <elliot@pourmand.com>
2017-03-25 19:18:06 -0700

    commit for life

--- TestSamples.py
+++ TestSamples.py
@@ -1,6 +1,7 @@
 import TurtleSamples
-
-
-TurtleSamples.FivePetal(5, "red", "black")
+#TurtleSamples.eight_petal()
+#TurtleSamples.eight_petal()
+#TurtleSamples.FivePetal(5, "red", "black")
 #TurtleSamples.FivePetal_Rainbow(.5)
 #TurtleSamples.Spiral(None, "pink")
+TurtleSamples.eight_petal_rainbow()
--- TurtleSamples.py
+++ TurtleSamples.py
@@ -4,10 +4,10 @@ RAINBOW = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#F
 
 
 
-def Spiral(spread=None, color=None):
-	if color == None:
+def spiral(spread=None, color=None):
+	if color is None:
 		color = "black"
-	if spread == None:
+	if spread is None:
 		spread = 4
 	ninja1 = turtle.Turtle()
 	ninja1.speed(100)
@@ -19,27 +19,27 @@ def Spiral(spread=None, color=None):
 	turtle.done()
 
 
-def SpiralRainbow(spread=None):
-	if spread == None:
+def spiral_rainbow(spread=None):
+	if spread is None:
 		spread = 4
 	ninja1 = turtle.Turtle()
 	ninja1.speed(100)
 	ninja1.color(color, "green")
-	k=0
+	k = 0
 	for i in range(10000):
 		if k > len(RAINBOW)-1:
 			k = 0
 		ninja1.color(RAINBOW[k])
-		k+=1
+		k += 1
 		ninja1.forward(i*spread)
 		ninja1.right(143)
 	turtle.done()
 
 
-def Spiral(spread=None, color=None):
-	if color == None:
+def spiral_hex(spread=None, color=None):
+	if color is None:
 		color = "black"
-	if spread == None:
+	if spread is None:
 		spread = 4
 	ninja1 = turtle.Turtle()
 	ninja1.speed(100)
@@ -54,12 +54,12 @@ def Spiral(spread=None, color=None):
 
 
 
-def FivePetal(spread=None, color1=None, color2=None):
-	if color1 == None:
+def five_petal(spread=None, color1=None, color2=None):
+	if color1 is None:
 		color1 = "black"
-	if color2 == None:
+	if color2 is None:
 		color2 = "black"
-	if spread == None:
+	if spread is None:
 		spread = 4
 	ninja1 = turtle.Turtle()
 	ninja2 = turtle.Turtle()
@@ -76,10 +76,10 @@ def FivePetal(spread=None, color1=None, color2=None):
 
 
 
-def FivePetal_Rainbow(spread=None, color=None):
-	if color == None:
+def five_petal_rainbow(spread=None, color=None):
+	if color is None:
 		color = "black"
-	if spread == None:
+	if spread is None:
 		spread = 4
 	ninja1 = turtle.Turtle()
 	ninja2 = turtle.Turtle()
@@ -97,5 +97,53 @@ def FivePetal_Rainbow(spread=None, color=None):
 			k = 0
 		#ninja1.color(RAINBOW[k])
 		ninja2.color(RAINBOW[k])
-		k+=1
+		k += 1
+	turtle.done()
+
+def eight_petal(spread=None, color1=None, color2=None):
+	if color1 is None:
+		color1 = "black"
+	if color2 is None:
+		color2 = "green"
+	if spread is None:
+		spread = 3
+	spiral = turtle.Turtle()
+	spiral2 = turtle.Turtle()
+	spiral.speed(1000)
+	spiral2.speed(1000)
+	spiral.color(color1)
+	spiral2.color(color2)
+	for i in range(1000000):
+		spiral.forward(i/spread)
+		spiral.right(45.2)
+		spiral2.forward(i/spread)
+		spiral2.left(45.2)
+	turtle.done()
+
+
+
+def eight_petal_rainbow(spread=None, color1=None, color2=None):
+	if color1 is None:
+		color1 = "black"
+	if color2 is None:
+		color2 = "green"
+	if spread is None:
+		spread = 3
+	spiral = turtle.Turtle()
+	spiral2 = turtle.Turtle()
+	spiral.speed(0)
+	spiral2.speed(0)
+	spiral.color(color1)
+	k = 0
+	for i in range(1000000):
+		spiral.forward(i/spread)
+		spiral.right(45.2)
+		spiral2.forward(i/spread)
+		spiral2.left(45.2)
+		if k > len(RAINBOW)-1:
+			k = 0
+
+		spiral.color(RAINBOW[k])
+		#ninja1.color(RAINBOW[k])
+		k += 1
 	turtle.done()
